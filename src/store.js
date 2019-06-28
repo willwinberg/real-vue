92f04a79874746f92a1d76a4fb7248b9711d2640
William <wtpwinberg@gmail.com>
2019-06-28 10:18:54 -0600

    complete state and getters chapter

--- src/store.js
+++ src/store.js
@@ -1,10 +1,43 @@
-import Vue from "vue";
-import Vuex from "vuex";
+import Vue from 'vue'
+import Vuex from 'vuex'
 
-Vue.use(Vuex);
+Vue.use(Vuex)
 
 export default new Vuex.Store({
-  state: {},
+  state: {
+    user: {
+      id: 'abc123',
+      name: 'Bill winnerberger'
+    },
+    categories: [
+      'sustainability',
+      'nature',
+      'animal welfare',
+      'housing',
+      'education'
+    ],
+    events: [
+      { id: 1, text: '...', done: true },
+      { id: 2, text: '...', done: false },
+      { id: 3, text: '...', done: true },
+      { id: 4, text: '...', done: false }
+    ]
+  },
   mutations: {},
-  actions: {}
-});
+  actions: {},
+  getters: {
+    categoriesLength: state => {
+      return state.categories.length
+    },
+    doneTodos: state => {
+      return state.todos.filter(todo => todo.done)
+    },
+    activeTodosCount: (state, getters) => {
+      // just an example. this would be bad prectice in production. expensive
+      return state.todos.length - getters.doneTodos.length
+    },
+    getEventById: state => id => {
+      return state.events.find(event => event.id === id)
+    }
+  }
+})
--- src/views/EventCreate.vue
+++ src/views/EventCreate.vue
@@ -1,13 +1,42 @@
 <template>
-  <h1>Event Create</h1>
+  <div>
+    <h1>Event Create, {{ user.name }}</h1>
+    <p>Event by, {{ user.id }}</p>
+    <p>There are {{ categoriesLength }} categories</p>
+    <p>{{ getEventById(2) }}</p>
+  </div>
 </template>
 
 <script>
+import { mapState, mapGetters } from 'vuex'
+
 export default {
-  data() {
-    return {
-      quote: 'I want to be created'
-    }
+  // computed: {
+  //   userName() {
+  //     return this.$store.state.user.name
+  //   },
+  //   userID() {
+  //     return this.$store.state.user.id
+  //   }
+  // }
+  // computed: mapState({
+  //   userName: state => state.user.name,
+  //   userID: state => state.user.id,
+  //   catagories: state => state.user.catagories
+  // })
+  // computed: mapState({
+  //   user1: 'user',
+  //   catagories1: 'catagories'
+  // })
+  computed: {
+    // getEventById() {
+    //   return this.$store.getters.getEventById
+    // },
+    categoriesLength() {
+      return this.$store.getters.categoriesLength
+    },
+    ...mapGetters(['getEventById']),
+    ...mapState(['user', 'catagories'])
   }
 }
 </script>
